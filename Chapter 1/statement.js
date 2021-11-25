6196b5881304c337b82a988c9570aec52fe74cf0
minbr0ther <minbr0ther@naver.com>
2021-11-25 10:17:59 +0900

    refactor(credateStatementData): 별도함수로 추출

--- Chapter 1/statement.js
+++ Chapter 1/statement.js
@@ -1,10 +1,6 @@
-let playsJson = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as-like': { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
-};
+import credateStatementData from './createStatementData.js';
 
-let invoicesJson = {
+const invoicesJson = {
   customer: 'BigCo',
   performances: [
     {
@@ -23,85 +19,7 @@ let invoicesJson = {
 };
 
 function statement(invoice) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount();
-  statementData.totalVolumeCredits = totalVolumeCredits();
-  return renderPlainText(statementData);
-
-  function enrichPerformance(aPerformance) {
-    //데이터 불변성을 위한 얕은 복사
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result); //중간 데이터에 연극 정보를 저장
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0; //변수 이름 바꾸기
-    for (let perf of invoicesJson.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0; //변수 이름 바꾸기
-    for (let perf of invoicesJson.performances) {
-      //값 누적 로직을 별도 for문으로 분리
-      //포인트를 적립한다.
-      result += perf.volumeCredits; //추출한 함수를 이용해 값을 누적
-    }
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    //renderPlainText에서 statement로 옮김
-    //임시 변수의 질의 함수로 바꾸기
-    return playsJson[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    //perf -> aPerformance : 접두어로 타입 이름 붙임
-    //"매개변수의 역할이 뚜렷하지 않을 때는 부정관사 (a/an)를 붙인다."
-    //"컴퓨터가 이해하는 코드는 바보도  작성할 수 있다. 사람이 이해하도록 작성하는 프로그래머가 진정한 실력자다."
-    //"좋은 코드라면 하는 일이 명확히 드러나야하며, 이때 변수 이름은 커다란 역할을 한다."
-    let result = 0; //thsAmount -> result
-
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': //희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result; //함수 안에서 값이 바뀌는 변수 반환
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-
-    //희극 관객 5명 마다 추가 포인트를 제공한다.
-    if ('comedy' === aPerformance.play.type) {
-      //playFor(aPerformance) 변수 인라인
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-    }
-    return volumeCredits;
-  }
+  return renderPlainText(credateStatementData(invoice));
 }
 
 function renderPlainText(data) {
