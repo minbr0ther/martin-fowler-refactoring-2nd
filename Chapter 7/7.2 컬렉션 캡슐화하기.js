9a9d1f3f9d4a138b92d9e68a2c21080d9768b62f
minbr0ther <minbr0ther@naver.com>
2021-12-02 20:37:09 +0900

    [8장 기능 이동] 7.2, 8.1

---
+++ Chapter 7/7.2 컬렉션 캡슐화하기.js
@@ -0,0 +1,50 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+    this._courses = [];
+  }
+  get name() {
+    return this._name;
+  }
+  get courses() {
+    return this._courses;
+  }
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
+  }
+}
+
+class Course {
+  constructor(name, isAdvanced) {
+    this._name = name;
+    this._isAdvanced = isAdvanced;
+  }
+  get name() {
+    return this._name;
+  }
+  get isAdvanced() {
+    return this._isAdvanced;
+  }
+}
+
+numAdvancedCourses = aPerson.courses.filter((c) => c.isAdvanced).length;
+
+const basicCourseNames = readBasicNames(filename);
+
+// Before
+aPerson.courses = basicCourseNames.map((name) => new Course(name, false));
+
+// After
+for (const name of readBasicCourseNames(filename)) {
+  aPerson.addCourse(new Course(name, false));
+}
